91f26f6e035fc36b50a5ba2e0ba18b6a31a62100
lss53 <2720626504@qq.com>
2024-03-17 22:04:33 +0800

    Add files via upload

---
+++ src/components/MoreContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="more-content">🎉欢迎光临👏</div>
+</template>
+
+<style lang="scss" scoped>
+.more-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  width: 100%;
+  height: 100%;
+}
+</style>
